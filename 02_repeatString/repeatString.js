67b1742646d779a144c26268314045feaab6a5f7
AndreyMpr <andreympr@gmail.com>
2024-08-25 12:33:38 +0900

    Commit changes before lunch.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function() {
-
+    let string = prompt("What do you want to repeat?");
+    let repeater = prompt("How many times?");
+    return string * parseInt(repeater);
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
